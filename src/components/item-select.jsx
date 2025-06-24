fdd0f6c257ea194f8c5b00811f784be6d506fa2b
penzboti <penzboti@gmail.com>
2025-06-24 22:54:15 +0200

    item selection box

---
+++ src/components/item-select.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import { Check, ChevronsUpDown } from "lucide-react"
+import { cn } from "../lib/utils"
+import { Button } from "./ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "./ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "./ui/popover";
+
+import { items } from "../scripts/module";
+
+function ItemSelect() {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+
+  function selectedItem() {
+    if (!value) return "Select item...";
+    const json = items.find((item) => item.id === value);
+    if (typeof json === "undefined") return "Item not found.";
+    return (<>
+      <img className="w-10 overflow-hidden mr-[10px]" src={json.image} alt={json.name} />
+      {json.name}
+    </>);
+  }
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[300px] justify-start"
+        >
+          { selectedItem() }
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command filter={(value, search, keywords) => {
+    const extendValue = value + ' ' + keywords.join(' ')
+    if (extendValue.includes(search)) return 1
+    return 0
+  }}>
+          <CommandInput placeholder="Search item..." className="h-9" />
+          <CommandList>
+            <CommandEmpty>No item found.</CommandEmpty>
+            <CommandGroup>
+              {items.map((item) => (
+                <CommandItem
+                  key={item.id}
+                  value={item.id}
+                  keywords={[item.name, item.name.toLowerCase(), item.id.toLowerCase()]}
+                  onSelect={(currentValue) => {
+                    setValue(currentValue === value ? "" : currentValue)
+                    setOpen(false)
+                  }}
+                >
+                  <img src={item.image} alt={item.name} className="w-10" />
+                  {item.name}
+                  <Check
+                    className={cn(
+                      "ml-auto",
+                      value === item.id ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
+
+export default ItemSelect;
--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -2,35 +2,27 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { items } from "../scripts/module";
 
-import { Separator } from "../components/ui/separator" // was @/components/ui/separator
+import { Separator } from "../components/ui/separator"; // was @/components/ui/separator
+import { Button } from "../components/ui/button";
+
+import ItemSelect from "../components/item-select";
 
 function Test() {
   let loc = location.href;
-  let [item_element, updateElement] = useState(<></>);
-
-  function displayItems() {
-    updateElement(items.map(item => (
-      <p
-        key={item.id}
-      >
-        <img src={item.image} className="w-20" />
-        {item.name}
-      </p>
-    )));
-  }
 
   return (
     <>
-      <Link to="/" style={{
-        color: 'pink'
-      }}>Link Back home</Link>
+      <Button asChild variant="secondary">
+        <Link to="/" style={{
+          color: 'pink'
+        }}>Link Back home</Link>
+      </Button>
       <h1>this is a test site</h1>
       <p>you are at: {loc}</p>
 
       <Separator />
 
-      <button onClick={displayItems}>Items</button>
-      {item_element}
+      <ItemSelect />
       <p>end</p>
     </>
   );
